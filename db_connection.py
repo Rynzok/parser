9d0612cc339f7cc1bb4cdf6ee1aae37a41a911dc
Rynzok <vkisel102@yandex.ru>
2025-03-25 21:32:37 +0300

    В теории должно заработать и вписать всё в бд

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import sql
+from psycopg2 import extras
 
 # Параметры подключения
 host = "localhost"  # или IP-адрес вашего сервера
@@ -8,7 +9,7 @@ user = "user"
 password = "user"
 
 
-def open_db_connection():
+def insert_first_list_in_db(insert_data):
     try:
         # Установка соединения
         connection = psycopg2.connect(
@@ -20,11 +21,78 @@ def open_db_connection():
 
         # Создание курсора для выполнения операций с базой данных
         cursor = connection.cursor()
+        table_name = 'main'
+        # Создание таблицы
+        create_table_query = f'''
+              CREATE TABLE IF NOT EXISTS {table_name} (
+                  id SERIAL PRIMARY KEY,
+                  key VARCHAR(100),
+                  value TEXT
+              );
+              '''
+        cursor.execute(create_table_query)
+        print(f"Таблица {table_name} создана или уже существует.")
 
-        # Пример выполнения запроса
-        cursor.execute("SELECT version();")
-        db_version = cursor.fetchone()
-        print("Вы подключены к - ", db_version)
+
+        # Формирование SQL-запроса для вставки нескольких строк
+        insert_data_query = f'''
+         INSERT INTO {table_name} (key, value) VALUES %s;
+         '''
+
+        # Используем psycopg2.extras для вставки нескольких строк
+
+        extras.execute_values(cursor, insert_data_query, insert_data)
+
+        print(f"Данные успешно вставлены в таблицу '{table_name}'.")
+
+        connection.commit()
+
+    except Exception as e:
+        print("Ошибка при подключении к PostgreSQL", e)
+
+    finally:
+        # Закрытие курсора и соединения
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+def insert_second_list_in_db(insert_data, table_name, reference_name):
+    try:
+        # Установка соединения
+        connection = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+
+        # Создание курсора для выполнения операций с базой данных
+        cursor = connection.cursor()
+        # Создание таблицы
+        create_table_query = f'''
+              CREATE TABLE IF NOT EXISTS {table_name} (
+                  id SERIAL PRIMARY KEY,
+                  key VARCHAR(100),
+                  value TEXT
+                  key_id INT REFERENCES {reference_name}(id)
+              );
+              '''
+        cursor.execute(create_table_query)
+        print(f"Таблица {table_name} создана или уже существует.")
+
+        # Формирование SQL-запроса для вставки нескольких строк
+        insert_data_query = f'''
+         INSERT INTO {table_name} (key, value, key_id) VALUES %s;
+         '''
+
+        # Используем psycopg2.extras для вставки нескольких строк
+
+        extras.execute_values(cursor, insert_data_query, insert_data)
+
+        print(f"Данные успешно вставлены в таблицу '{table_name}'.")
+
+        connection.commit()
 
     except Exception as e:
         print("Ошибка при подключении к PostgreSQL", e)
@@ -37,4 +105,43 @@ def open_db_connection():
             connection.close()
 
 
-open_db_connection()
+
+        # table_name_2 = 'company'
+        # # Создание таблицы
+        # create_table_query = f'''
+        #       CREATE TABLE IF NOT EXISTS {table_name_2} (
+        #           id SERIAL PRIMARY KEY,
+        #           key_name VARCHAR(100),
+        #           value VARCHAR(100),
+        #           key_id INT REFERENCES {table_name}(id)
+        #       );
+        #       '''
+        # cursor.execute(create_table_query)
+        # print("Таблица 'company' создана или уже существует.")
+
+
+
+        # # Вставка данных в таблицу
+        # data_to_insert = [
+        #     ('045', 'бери', 1),
+        #     ('3765848', 'что хочешь', 1),
+        #     ('23', 'но не меня', 1)
+        # ]
+        #
+        # # Формирование SQL-запроса для вставки нескольких строк
+        # insert_data_query = f'''
+        #  INSERT INTO {table_name_2} (key_name, value, key_id) VALUES %s;
+        #  '''
+        #
+        # # Используем psycopg2.extras для вставки нескольких строк
+        #
+        # extras.execute_values(cursor, insert_data_query, data_to_insert)
+        #
+        # print(f"Данные успешно вставлены в таблицу '{table_name_2}'.")
+        #
+        #
+        # select_query = f'SELECT * FROM {table_name_2};'
+        # cursor.execute(select_query)
+        # rows = cursor.fetchall()
+        # for row in rows:
+        #     print(row)
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from db_connection import insert_first_list_in_db, insert_second_list_in_db
 import time
 
 
-def create_dict(x, y):
-    result_dict = {}
-    # print('dfgh')
+def create_list_of_tuples(x, y):
+    result_list = []
     for j in range(min(len(x), len(y))):
-        key = code[j].text
-        value = description[j].text
-        result_dict[key] = value
-    return result_dict
+        key = x[j].text  # Используем x для ключа
+        value = y[j].text  # Используем y для значения
+        result_list.append((key, value))  # Добавляем кортеж (key, value) в список
+    return result_list
 
 
-def create_dict2(elements):
-    # print('create_dict2')
-    result_dict = {}
+def create_list_of_lists(elements, index):
+    result_list = []
 
-    # Проходим по элементам с шагом 2
+    # Проходим по элементам
     for el in elements:
         parts = el.text.split('\n')
 
@@ -25,11 +24,20 @@ def create_dict2(elements):
         if len(parts) >= 2:
             key = parts[0].strip()
             value = parts[1].strip()
-            result_dict[key] = value
+            # Добавляем список из key, value и id
+            result_list.append([key, value, index])  # Используем index как id
 
-    return result_dict
+    return result_list
 
-def clic_click(click_element, id):
+def parse_data_to_list(data, fkey):
+    result_list = []
+    for line in data.strip().split('\n'):
+        key, value = line.split(' - ', 1)  # Разделяем строку на ключ и значение
+        result_list.append([key, value.strip(), fkey])  # Убираем лишние пробелы и добавляем в словарь
+    return result_list
+
+def clic_click(ref_name, click_element, element_id):
+    t_name = click_element.text
     click_element.click()
     # print("clic_click")
     # time.sleep(1)
@@ -38,20 +46,19 @@ def clic_click(click_element, id):
     length = len(href)
     print('length: '+ str(length))
     if key :
-        print(create_dict2(key))
+        result_list = create_list_of_lists(key, id)
+        insert_second_list_in_db(result_list, t_name, ref_name)
         for j in range(length):
             print('j: ' + str(j))
             href = driver.find_elements(By.CSS_SELECTOR, 'a.mktu-category__class-link.blue-link')
-            clic_click(href[j], j)
+
+            clic_click(href[j].text ,href[j], j + 1, )
         driver.back()
 
     elif driver.find_elements(By.CLASS_NAME, 'mktu-class__content'):
         data = driver.find_element(By.CLASS_NAME, 'mktu-class__content').text
-        result_dict = {}
-        for line in data.strip().split('\n'):
-            key, value = line.split(' - ', 1)
-            result_dict[key] = value.strip()
-        print(result_dict)
+        result_list = parse_data_to_list(data, element_id)
+        insert_second_list_in_db(result_list, t_name, ref_name)
         driver.back()
     else:
         driver.back()
@@ -73,17 +80,21 @@ try:
     description = driver.find_elements(By.CLASS_NAME, 'mktu-category__class-discr')
     # Создание словаря
 
-    all_dict = create_dict(code, description)
+    all_list = create_list_of_tuples(code, description)
+    insert_first_list_in_db(all_list)
 
-    print(all_dict)
+
+    # print(all_list)
+    table_name = 'main'
 
     first_length = len(code)
     print(first_length)
+
     for i in range(first_length):
         time.sleep(1)
         code2 = driver.find_elements(By.CLASS_NAME, 'mktu-category__class-link')
         print('INDEX: ' + str(i) + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        clic_click(code2[i], i)
+        clic_click(table_name, code2[i], i + 1)
 
 
 except Exception as e:
